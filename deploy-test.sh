c17b01e0fb7c0c2dac6998c7078126d7e625dfca
Tom Trahan <tom@shippable.com>
2016-10-10 08:59:11 -0700

    add comments to deploy-test.sh file

--- deploy-test.sh
+++ deploy-test.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -e
 
+# 1. Install required tools
 echo -e "\n*** apt-get update ***"
 sudo apt-get update
 
 echo -e "\n*** installing jq ***"
 sudo apt-get install jq
 
+# 2. Extract state from inputs to the runSh job that calls this script
 echo -e "\n*** extracting manifest and params information ***"
 MAN_FILE=IN/helloapp-man/manifestSteps/manifests.json
 export PARAMS=$(jq -r '.[].images[].params' $MAN_FILE)
-export MARATHON_APP_FILE=$(jq -r '.[].images[].params.MARATHON_APP_FILE' $MAN_FILE)
 export ENVIRONMENT=$(jq -r '.[].images[].params.ENVIRONMENT' $MAN_FILE)
+export MARATHON_APP_FILE=$(jq -r '.[].images[].params.MARATHON_APP_FILE' $MAN_FILE)
 export MARATHON_HOST=$(jq -r '.[].images[].params.MARATHON_HOST' $MAN_FILE)
 export MESOS_APP_NAME=$(jq -r '.[].images[].params.MESOS_APP_NAME' $MAN_FILE)
 export IMAGE_NAME=$(jq -r '.[].images[].image' $MAN_FILE)
@@ -20,11 +22,12 @@ export CPU_SHARES=$(jq -r '.[].images[].dockerOptions.cpuShares' $MAN_FILE)
 export PORT_MAPPINGS=$(jq -r '.[].images[].dockerOptions.portMappings[]' $MAN_FILE)
 export REPLICAS=$(jq -r '.[].replicas' $MAN_FILE)
 
+# 3. Update the Marathon deployment specification
 echo -e "\n*** updating $MARATHON_APP_FILE ***"
 cd IN/helloapp-scripts/gitRepo
 python version.py
-cat marathon.json
 
+# 4. Deploy to Marathon
 echo "\n*** deploying Application $MESOS_APP_NAME to $MARATHON_HOST ***"
 APP_EXISTS=$(curl -s -o /dev/null -w "%{http_code}" ${MARATHON_HOST}/v2/apps/${MESOS_APP_NAME})
 
@@ -45,4 +48,5 @@ else
   exit 1
 fi
 
+# 5. Finish processing this script
 echo -e "\n*** processing complete - deploy-test.sh ***"
